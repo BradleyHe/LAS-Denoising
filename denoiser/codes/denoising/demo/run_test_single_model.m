674ef3b2cb12f9d9d1402623fea20f2a376dd6f3
Bradley H <bhe6001@gmail.com>
2019-11-13 09:04:37 -0500

    add tir

--- denoiser/codes/denoising/demo/run_test_single_model.m
+++ denoiser/codes/denoising/demo/run_test_single_model.m
@@ -35,22 +35,29 @@ function run_test_single_model
     index = 2;
     [speech, fs] = audioread(['wav', filesep, 'original_speech', num2str(index), '.wav']);
     [noise, fs] = audioread(['wav', filesep, 'original_noise',num2str(index),'.wav']);
+    
+    r = snr(speech, noise);
+    factor = 10^((3-r)/20);
 
+    r = snr(speech, noise / factor);
+    disp(r);
+    
     x = speech + noise;    
     eI.fs = fs;
+    
+    audiowrite([eI.saveDir, filesep, 'noisy_speech', '.wav'], x, fs);
+    
     %%
     output = test_denoising_general_kl_bss3(x', theta, eI, 'testall', 0);
     %%
   	sz = 1024.*[1 1/4];
     wn = sqrt( hann( sz(1), 'periodic')); % hann window
-    wav_singal = stft2( output.source_signal, sz(1), sz(2), 0, wn);
+    wav_signal = stft2( output.source_signal, sz(1), sz(2), 0, wn);
     wav_noise = stft2( output.source_noise, sz(1), sz(2), 0, wn);
-    wav_singal = wav_singal./max(abs(wav_singal));
+    wav_signal = wav_signal./max(abs(wav_signal));
     wav_noise = wav_noise./max(abs(wav_noise));    
 
-    audiowrite([eI.saveDir, filesep,'separated_speech',num2str(index),'.wav'], wav_singal, fs);
-    audiowrite([eI.saveDir, filesep,'separated_noise',num2str(index),'.wav'], wav_noise, fs);
-    
-    % Get separation stats
-    [sdr,sir,sar,stoi] = sep_perf(wav_singal, [speech'; noise'], fs);    
+    audiowrite([eI.saveDir, filesep,'separated_speech',num2str(index),'.wav'], wav_signal, fs);
+    % audiowrite([eI.saveDir, filesep,'separated_noise',num2str(index),'.wav'], wav_noise, fs);
+        
 end
