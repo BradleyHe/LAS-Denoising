f677adb923fc3f266028ad9f58d9cf21115303db
Bradley <bhe6001@gmail.com>
2020-01-23 21:49:07 -0500

    show me the data

---
+++ las/calc.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
+
+# generated 33 data files
+data_list = []
+df = pd.DataFrame()
+
+for x in range(1, 11):
+	df = df.append(pd.read_csv('pkl/{}_denoised.csv'.format(x), index_col=0))
+
+data_mean = df.groupby(level=0).mean()
+data_std = df.groupby(level=0).std()
+data_mean.to_csv('data_mean_denoised.csv')
+data_std.to_csv('data_std_denoised.csv')
+
+print(data_mean)
+print(data_std)
+
+# for category in categories:
+# 	fig = plt.figure()
+# 	ax = fig.add_subplot(1, 1, 1)
+
+# 	plt.plot(list(data_mean.index), data_mean[category], color='k', linewidth=1, marker='o', mfc='g', clip_on=False, zorder=100)
+# 	plt.errorbar(data_mean.index, data_mean[category], fmt='none', ecolor='k', capsize=4, yerr=data_std[category])
+
+# 	ax.set_xlim(0, 12)
+# 	ax.set_ylim(0.27, 0.78)
+# 	ax.set_zorder(-5)
+
+# 	plt.setp(ax.spines.values(), linewidth=2)
+# 	plt.grid(True)
+# 	plt.axhline(y=0.26, color='k', linestyle='--')
+
+# 	plt.xticks(range(0, 33, 3))
+# 	plt.yticks(np.arange(0.25, 0.8, 0.05))
+# 	plt.xlabel('TIR (dB)')
+# 	plt.ylabel('PER')
+# 	plt.legend((category[0] + '-' + category[1], 'Unmodified'), loc='upper right')
+# 	ax.set_title('Performance with ' + category[0] + '-' + category[1]  + ' mixing', fontdict={'fontsize': 10, 'fontweight': 'medium'})
+# 	plt.savefig(category + '.pdf', bbox_inches = 'tight', pad_inches = 0)
--- las/test_timit_mixed.py
+++ las/test_timit_mixed.py
@@ -8,6 +8,8 @@ from torch.autograd import Variable
 import sys
 import pandas as pd
 from six.moves import cPickle
+import os
+
 
 def load_dataset(data_path,**kwargs):
     with open(data_path, 'rb') as cPickle_file:
@@ -17,34 +19,37 @@ def load_dataset(data_path,**kwargs):
     return X_test, y_test
 
 config_path = 'config/las_example_config.yaml'
-tir = [-10, -5, 0, 5, 10, 15, 20]
+tir = [-20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30]
 
 conf = yaml.load(open(config_path,'r'))
-data = {}
+os.makedirs('pkl', exist_ok=True)   
 
-for ratio in tir:
-    global_step = 0
-    total = 0
+for category in ['noisy', 'denoised']:
+    for num in range (1, 11):
+        data = {}
+        for ratio in tir:
+            global_step = 0
+            total = 0
 
-    conf['meta_variable']['data_path'] = 'timit_mfcc_39_{}.pkl'.format(ratio)
+            conf['meta_variable']['data_path'] = 'pkl/{}/timit_mfcc_39_{}_{}.pkl'.format(num, category, ratio)
 
-    X_test, y_test = load_dataset(**conf['meta_variable'])
-    test_set = create_dataloader(X_test, y_test, **conf['model_parameter'], **conf['training_parameter'], shuffle=False)
+            X_test, y_test = load_dataset(**conf['meta_variable'])
+            test_set = create_dataloader(X_test, y_test, **conf['model_parameter'], **conf['training_parameter'], shuffle=False)
 
-    listener = torch.load(conf['training_parameter']['pretrained_listener_path'], map_location=lambda storage, loc: storage)
-    speller = torch.load(conf['training_parameter']['pretrained_speller_path'], map_location=lambda storage, loc: storage)
-    optimizer = torch.optim.Adam([{'params':listener.parameters()}, {'params':speller.parameters()}], lr=conf['training_parameter']['learning_rate'])
+            listener = torch.load(conf['training_parameter']['pretrained_listener_path'], map_location=lambda storage, loc: storage)
+            speller = torch.load(conf['training_parameter']['pretrained_speller_path'], map_location=lambda storage, loc: storage)
+            optimizer = torch.optim.Adam([{'params':listener.parameters()}, {'params':speller.parameters()}], lr=conf['training_parameter']['learning_rate'])
 
-    for batch_index,(batch_data,batch_label) in enumerate(test_set):
-      batch_loss, batch_ler = batch_iterator(batch_data, batch_label, listener, speller, optimizer, 0, is_training=False, **conf['model_parameter'])
-      total += np.array(batch_ler).mean()
-      global_step += 1
-      print('PER for {}: {}'.format(ratio, total / global_step), end='\r', flush=True)
+            for batch_index,(batch_data,batch_label) in enumerate(test_set):h
+              batch_loss, batch_ler = batch_iterator(batch_data, batch_label, listener, speller, optimizer, 0, is_training=False, **conf['model_parameter'])
+              total += np.array(batch_ler).mean()
+              global_step += 1
+              print('PER for {}: {}'.format(ratio, total / global_step), end='\r', flush=True)
 
-    data[ratio] = [total / global_step]
-    print()
-    df = pd.DataFrame(data)
-    df.to_csv('per_data.csv')
-df = pd.DataFrame(data)
-df.to_csv('per_data.csv')
-print(df)
+            data[ratio] = [total / global_step]
+            print()
+
+            df = pd.DataFrame(data)
+            df.to_csv('pkl/{}_{}.csv'.format(num, category))
+        df = pd.DataFrame(data)
+        df.to_csv('pkl/{}_{}.csv'.format(num, category))
