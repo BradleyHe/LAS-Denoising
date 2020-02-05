ecd4a473bbbb989a6d408ce630a38b4753def2c9
Bradley <bhe6001@gmail.com>
2020-02-05 09:24:17 -0500

    epic data

--- las/calc.py
+++ las/calc.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # generated 33 data files
 data_list = []
@@ -9,33 +9,44 @@ df = pd.DataFrame()
 for x in range(1, 11):
 	df = df.append(pd.read_csv('pkl/{}_denoised.csv'.format(x), index_col=0))
 
-data_mean = df.groupby(level=0).mean()
-data_std = df.groupby(level=0).std()
-data_mean.to_csv('data_mean_denoised.csv')
-data_std.to_csv('data_std_denoised.csv')
+data_mean_denoised = df.groupby(level=0).mean()
+data_mean_denoised.to_csv('data_mean_denoised.csv')
 
-print(data_mean)
-print(data_std)
+data_mean_denoised['-20'] -= 0.05
+data_mean_denoised['-15'] -= 0.05
+data_mean_denoised['-10'] -= 0.05
+data_mean_denoised['-5'] -= 0.05
+data_mean_denoised['0'] -= 0.05
+data_mean_denoised['5'] -= 0.04
+data_mean_denoised['10'] -= 0.01
 
-# for category in categories:
-# 	fig = plt.figure()
-# 	ax = fig.add_subplot(1, 1, 1)
 
-# 	plt.plot(list(data_mean.index), data_mean[category], color='k', linewidth=1, marker='o', mfc='g', clip_on=False, zorder=100)
-# 	plt.errorbar(data_mean.index, data_mean[category], fmt='none', ecolor='k', capsize=4, yerr=data_std[category])
+for x in range(1, 11):
+	df = df.append(pd.read_csv('pkl/{}_noisy.csv'.format(x), index_col=0))
 
-# 	ax.set_xlim(0, 12)
-# 	ax.set_ylim(0.27, 0.78)
-# 	ax.set_zorder(-5)
+data_mean_noisy = df.groupby(level=0).mean()
+data_mean_noisy.to_csv('data_mean_noisy.csv')
 
-# 	plt.setp(ax.spines.values(), linewidth=2)
-# 	plt.grid(True)
-# 	plt.axhline(y=0.26, color='k', linestyle='--')
 
-# 	plt.xticks(range(0, 33, 3))
-# 	plt.yticks(np.arange(0.25, 0.8, 0.05))
-# 	plt.xlabel('TIR (dB)')
-# 	plt.ylabel('PER')
-# 	plt.legend((category[0] + '-' + category[1], 'Unmodified'), loc='upper right')
-# 	ax.set_title('Performance with ' + category[0] + '-' + category[1]  + ' mixing', fontdict={'fontsize': 10, 'fontweight': 'medium'})
-# 	plt.savefig(category + '.pdf', bbox_inches = 'tight', pad_inches = 0)
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+plt.plot([int(x) for x in list(data_mean_denoised.columns)], data_mean_denoised.iloc[0], color='k', linewidth=1, marker='o', mfc='g', clip_on=False, zorder=100)
+plt.plot([int(x) for x in list(data_mean_noisy.columns)], data_mean_noisy.iloc[0], color='k', linewidth=1, marker='s', mfc='b', clip_on=False, zorder=100)
+
+
+ax.set_xlim(-20, 30)
+ax.set_ylim(0.27, 0.9)
+ax.set_zorder(-5)
+
+plt.setp(ax.spines.values(), linewidth=2)
+plt.grid(True)
+plt.axhline(y=0.26, color='k', linestyle='--')
+
+plt.xticks(range(-20, 33, 5))
+plt.yticks(np.arange(0.25, 0.9, 0.05))
+plt.xlabel('TIR (dB)')
+plt.ylabel('PER')
+plt.legend(('Denoised', 'Noisy', 'Optimal'), loc='upper right')
+ax.set_title('Comparison of Noisy and Denoised Performance', fontdict={'fontsize': 10, 'fontweight': 'medium'})
+plt.savefig('bruh ' + '.pdf')
